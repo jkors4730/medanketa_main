236b2f6f4c42dd6fd9a1115a602df052548c8c84
jkors4730 <larnikko1@yandex.ru>
2024-12-04 00:13:17 +0300

    fix views path

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../assets')));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use('/', mainRoutes);
