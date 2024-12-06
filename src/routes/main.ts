16dfbdd9507aba330fe47d3dcaa1e5df1820365a
jkors4730 <larnikko1@yandex.ru>
2024-12-07 02:42:32 +0300

    env: lk_link

--- src/routes/main.ts
+++ src/routes/main.ts
@@ -2,24 +2,26 @@ import { Router, Request, Response } from 'express';
 
 const mainRoutes = Router();
 
+const lk = { lk_link: process.env.LK_LINK };
+
 mainRoutes.get('/', async (_req: Request, res: Response) => {
-    res.render('page', { page: 'main', title: 'Главная - Medanketa' });
+    res.render('page', { page: 'main', title: 'Главная - Medanketa', ...lk });
 });
 
 mainRoutes.get('/about', async (_req: Request, res: Response) => {
-    res.render('page', { page: 'about', title: 'О нас - Medanketa' });
+    res.render('page', { page: 'about', title: 'О нас - Medanketa', ...lk });
 });
 
 mainRoutes.get('/contacts', async (_req: Request, res: Response) => {
-    res.render('page', { page: 'contacts', title: 'Контакты - Medanketa' });
+    res.render('page', { page: 'contacts', title: 'Контакты - Medanketa', ...lk });
 });
 
 mainRoutes.get('/handbooks', async (_req: Request, res: Response) => {
-    res.render('page', { page: 'handbooks', title: 'Справочники - Medanketa' });
+    res.render('page', { page: 'handbooks', title: 'Справочники - Medanketa', ...lk });
 });
 
 mainRoutes.get('/policy', async (_req: Request, res: Response) => {
-    res.render('page', { page: 'policy', title: 'Политика обработки персональных данных - Medanketa' });
+    res.render('page', { page: 'policy', title: 'Политика обработки персональных данных - Medanketa', ...lk });
 });
 
 export default mainRoutes;
